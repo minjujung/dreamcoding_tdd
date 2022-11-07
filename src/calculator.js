ae708e6c0f35fd3050d447519f58587e5d4f5765
minjujung <mj.jung@teamsparta.co>
2022-11-08 07:26:52 +0900

    add: calculator.js 관련 test 코드 수업 내용 추가

--- src/calculator.js
+++ src/calculator.js
@@ -14,12 +14,12 @@ class Calculator {
   add(num) {
     const sum = this.value + num;
     if (sum > 100) {
-      throw new Error('Value can not be greater than 100');
+      throw new Error("Value can not be greater than 100");
     }
     this.value = sum;
   }
 
-  subtract(num) {
+  substract(num) {
     this.value = this.value - num;
   }
 
--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,14 +1,71 @@
 const Calculator = require("../calculator.js");
 
 describe("Calculator", () => {
+  let cal;
+
+  /**
+   * 모든 test 시작 전에 실행하는 함수
+   * beforeEach
+   */
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
   it("init with 0", () => {
-    const cal = new Calculator();
     expect(cal.value).toBe(0);
   });
 
   it("sets", () => {
-    const cal = new Calculator();
     cal.set(2);
     expect(cal.value).toBe(2);
+  });
+
+  it("clear", () => {
+    cal.set(2);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("add", () => {
+    cal.set(3);
+    cal.add(2);
+    expect(cal.value).toBe(5);
+  });
+
+  it("add should throw error when num > 100", () => {
+    expect(() => {
+      cal.add(101);
+    }).toThrow("Value can not be greater than 100");
+  });
+
+  it("substract", () => {
+    cal.set(3);
+    cal.substract(4);
+    expect(cal.value).toBe(-1);
+  });
+
+  it("multiply", () => {
+    cal.set(5);
+    cal.multiply(2);
+    expect(cal.value).toBe(10);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("normal case 4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
   });
 });
